8c97e0f9966b1be047b02dfad373d3666f2affd5
davidgfb <davidgracia99@gmail.com>
2023-09-27 20:34:24 +0200

    Update BA.glsl

--- BA.glsl
+++ BA.glsl
@@ -1,90 +1,100 @@
+const int idxCenter = 0, idxUpperBody = 1, idxNeck = 2, idxHead = 3,           
+          idxRightShoulder = 4, idxRightUpperArm = 5, 
+          idxRightLowerArm = 6, idxRightHand = 7, idxLeftShoulder = 8,
+          idxLeftUpperArm = 9, idxLeftLowerArm = 10, idxLeftHand = 11,
+          idxLowerBody = 12, idxRightHip = 13, idxRightUpperLeg = 14,
+          idxRightLowerLeg = 15, idxRightFoot = 16, idxLeftHip = 17,
+          idxLeftUpperLeg = 18, idxLeftLowerLeg = 19, idxLeftFoot = 20,
+          idxModelPos = 21, idxNone = 256;
 
-#define idxCenter         0
-#define idxUpperBody      1
-#define idxNeck           2
-#define idxHead           3
-#define idxRightShoulder  4
-#define idxRightUpperArm  5
-#define idxRightLowerArm  6
-#define idxRightHand      7
-#define idxLeftShoulder   8
-#define idxLeftUpperArm   9
-#define idxLeftLowerArm  10
-#define idxLeftHand      11
-#define idxLowerBody     12
-#define idxRightHip      13
-#define idxRightUpperLeg 14
-#define idxRightLowerLeg 15
-#define idxRightFoot     16
-#define idxLeftHip       17
-#define idxLeftUpperLeg  18
-#define idxLeftLowerLeg  19
-#define idxLeftFoot      20
-#define idxModelPos      21
-#define idxNone         256
+const vec3 x = vec3(1, 0, 0), y = vec3(0, 1, 0), modelPos = 5.0 * y; 
 
-#define modelPos vec3(0,5,0)
-
-vec4 quat(vec3 p, float a) //quaternion
-{
-    p=normalize(p);
-	return vec4(p*sin(a/2.0), cos(a/2.0));
+vec4 quat(vec3 p, float a) { //quaternion
+    p = normalize(p);
+	
+    return vec4(p * sin(a / 2.0), cos(a / 2.0));
 }
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-  	int id = int(fragCoord.x) + int(iResolution.y) * int(fragCoord.y);
-  	if (id > idxModelPos) discard;  
-	vec4 Q = vec4(0,0,0,1);
+void mainImage(out vec4 fragColor, vec2 fragCoord) {
+  	int id = int(iResolution.y) * int(fragCoord.y) + int(fragCoord.x);
+  	
+    //if (id > idxModelPos) discard;  
+	
+    float t = iTime * 0.3, a = abs(sin(iTime + 1.0)), s = sin(iTime), 
+          a1 = abs(s); 
     
-  	switch(id)
-  	{
+    vec4 Q = vec4(0, 0, 0, 1), 
+         Q1 = quat(vec3(0, sin(t), cos(t)), sin(iTime * 3.0) * 0.8),
+         Q2 = quat(x, -0.5);
+    
+    switch (id) {
 		case idxCenter:
-    		Q = quat(vec3(1),(iTime)*0.3);
-      		break;
- 		case idxUpperBody:
-    		Q = quat(vec3(1,0,0),sin(iTime)*0.2+0.15);
-      		break;
+    		Q = quat(vec3(1), t);
+      		
+            break;
+ 		
+        case idxUpperBody:
+    		Q = quat(x, s / 5.0 + 0.15);
+      		
+            break;
  		
         case idxRightUpperArm:
-        	Q = quat(vec3(0,sin(iTime*0.3),cos(iTime*0.3)),sin(iTime*3.)*0.8);
-      		break;
-      	case idxRightLowerArm:
-    		Q = quat(vec3(0,1,0),abs(sin(iTime)));
-      		break;
+            Q = Q1;
+      		
+            break;
+      	
+        case idxRightLowerArm:
+    		Q = quat(y, a1);
+      		
+            break;
       	
         case idxLeftUpperArm:
-       		//Q = quat(vec3(0,0,1),radians(-90.));
-       		Q = quat(vec3(0,sin(iTime*0.3),cos(iTime*0.3)),sin(iTime*3.)*0.8);
-        	break;
-      	case idxLeftLowerArm:
-    		Q = quat(vec3(0,1,0),-abs(sin(iTime)));
-      		break;
+       		//Q = quat(vec3(0,0,1), radians(-90.0));            
+       		Q = Q1;
+        	
+            break;
+      	
+        case idxLeftLowerArm:
+    		Q = quat(y, -a1);
+      		
+            break;
       	
         case idxRightUpperLeg:
-        	Q = quat(vec3(1,0,-0.3),abs(sin(iTime+1.0)));
-        	break;
-      	case idxRightLowerLeg:
-       		Q = quat(vec3(1,0,0),-abs(sin(iTime+1.0)));
-        	break;
-      	case idxRightFoot:
-       		Q = quat(vec3(1,0,0),-0.5);
-        	break;
-      	case idxLeftHip:
-        	break;
-      	case idxLeftUpperLeg:
-        	Q = quat(vec3(1,0,0.3),abs(sin(iTime)));
-        	break;
-      	case idxLeftLowerLeg:
-       		Q = quat(vec3(1,0,0),-abs(sin(iTime)));
-        	break;
-      	case idxLeftFoot:
-       		Q = quat(vec3(1,0,0),-0.5);
-        	break;
-      	case idxModelPos:
+        	Q = quat(vec3(1, 0, -0.3), a);
+        	
+            break;
+      	
+        case idxRightLowerLeg:
+       		Q = quat(x, -a);
+        	
+            break;
+      	
+        case idxRightFoot:
+       		Q = Q2;
+        	
+            break;
+      	
+        case idxLeftUpperLeg:
+        	Q = quat(vec3(1, 0, 0.3), a1);
+        	
+            break;
+      	
+        case idxLeftLowerLeg:
+       		Q = quat(x, -a1);
+        	
+            break;
+      	
+        case idxLeftFoot:
+       		Q = Q2;
+        	
+            break;
+      	
+        case idxModelPos:
           	Q.xyz = modelPos;
-       		break;
+       		
+            break;
 	}
-	//Q = vec4(0,0,0,1);
-    fragColor = vec4(Q);  
+	
+    //Q = vec4(0,0,0,1);
+    fragColor = Q;  
 }
